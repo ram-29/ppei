4440ba6d9b9447e4f2a2e51236d5788197be0008
ram-29 <ram.delatina.29@gmail.com>
2017-11-22 13:29:52 +0800

    Added eslint in development workflow

--- frontend/design/scripts/app.js
+++ frontend/design/scripts/app.js
@@ -1,225 +1,230 @@
-; (function ($) {
-	// Banner Image Responsiveness
-	if ($('#banner').length) {
-		var bannerSlider = $('#banner-slider')
-		var bannerContent = $('#banner-content')
-		var images = bannerSlider.children().children().children().children()
-		images.map(i => images[i].style.height = bannerContent[0].clientHeight + 'px')
+(function ($) {
+  // Banner Image Responsiveness
+  if ($('#banner').length) {
+    const bannerSlider = $('#banner-slider');
+    const bannerContent = $('#banner-content');
+    const images = bannerSlider.children().children().children().children();
 
-		$(window).resize(function () {
-			images.map(i => images[i].style.height = bannerContent[0].clientHeight + 'px')
-		})
-	}
+    const imageResize = function () {
+      Object.keys(images).forEach((i) => {
+        !isNaN(i) ? images[i].style.height = `${bannerContent[0].clientHeight}px` : '';
+      });
+    };
 
-	// Tooltip
-	if ($('[data-toggle="tooltip"]').length) {
-		$('[data-toggle="tooltip"]').tooltip()
-	}
+    imageResize();
+    $(window).resize(() => imageResize());
+  }
 
-	// FB Share & Tweet
-	if ($('.list-group').length) {
-		$(".list-group").on("click", function (e) {
-			const li = e.target
-			let title, summary
+  // Tooltip
+  if ($('[data-toggle="tooltip"]').length) {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
 
-			if (li.className.includes("facebook")) {
-				getNewsTitleAndContent()
-				share(title, summary)
-			} else if (li.className.includes("twitter")) {
-				getNewsTitleAndContent()
-				tweet(title, summary)
-			}
+  // FB Share & Tweet
+  if ($('.list-group').length) {
+    $('.list-group').on('click', (e) => {
+      const li = e.target;
+      let title,
+        summary;
 
-			function getNewsTitleAndContent() {
-				const p = $(li).parent().prev()
-				const h3 = $(p).prev()
+      const getNewsTitleAndContent = function () {
+        const p = $(li).parent().prev();
+        const h3 = $(p).prev();
 
-				title = h3.text()
-				summary = p.text()
-			}
+        title = h3.text();
+        summary = p.text();
+      };
 
-			function share(title, summary) {
-				console.log(`SHARE\nTitle: ${title}\nContent: ${summary}`)
-			}
+      const share = function (mTitle, mSummary) {
+        console.log(`SHARE\nTitle: ${mTitle}\nContent: ${mSummary}`);
+      };
 
-			function tweet(title, summary) {
-				console.log(`TWEET\nTitle: ${title}\nContent: ${summary}`)
-			}
-		})	
-	}
+      const tweet = function (mTitle, mSummary) {
+        console.log(`TWEET\nTitle: ${mTitle}\nContent: ${mSummary}`);
+      };
 
-	// Fab Scroll Listener
-	if ($('#components-fab').length) {
-		var scrollPos = 0
-		var ticking = false
-		var fab = $('#components-fab')
+      if (li.className.includes('facebook')) {
+        getNewsTitleAndContent();
+        share(title, summary);
+      } else if (li.className.includes('twitter')) {
+        getNewsTitleAndContent();
+        tweet(title, summary);
+      }
+    });
+  }
 
-		function toggleFab(scroll_pos) {
-			if (scroll_pos) {
-				fab.addClass('fab-show')
-				return
-			}
-			fab.removeClass('fab-show')
-		}
+  // Fab Scroll Listener
+  if ($('#components-fab').length) {
+    let scrollPos = 0;
+    let ticking = false;
+    const fab = $('#components-fab');
 
-		window.addEventListener('scroll', function () {
-			scrollPos = window.scrollY
-			if (!ticking) {
-				window.requestAnimationFrame(function () {
-					toggleFab(scrollPos)
-					ticking = false
-				})
-				ticking = true
-			}
-		})
+    const toggleFab = function (mScrollPos) {
+      if (mScrollPos) {
+        fab.addClass('fab-show');
+        return;
+      }
+      fab.removeClass('fab-show');
+    };
 
-		var page = $('html, body')
-		var up = $(fab).children().first()
-		var down = $(up).next().next()
+    window.addEventListener('scroll', () => {
+      scrollPos = window.scrollY;
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          toggleFab(scrollPos);
+          ticking = false;
+        });
+        ticking = true;
+      }
+    });
 
-		up.click(function () {
-			page.animate({
-				scrollTop: 0
-			}, 500)
-		})
+    const page = $('html, body');
+    const up = $(fab).children().first();
+    const down = $(up).next().next();
 
-		down.click(function () {
-			page.animate({
-				scrollTop: page[1].clientHeight
-			}, 500)
-		})
-	}
+    up.click(() => {
+      page.animate({
+        scrollTop: 0,
+      }, 500);
+    });
 
-	// CLNDRjs
-	if ($('#mini-clndr').length) {
-		// MomentJS Hack : Deprecation Issue
-		moment.createFromInputFallback = function (config) { config._d = new Date(config._i) }
+    down.click(() => {
+      page.animate({
+        scrollTop: page[1].clientHeight,
+      }, 500);
+    });
+  }
 
-		var currentMonth = moment().format("YYYY-MM")
-		var nextMonth = moment().add(1, "month").format("YYYY-MM")
+  // CLNDRjs
+  if ($('#mini-clndr').length) {
+    // MomentJS Hack : Deprecation Issue
+    moment.createFromInputFallback = function (config) { config._d = new Date(config._i); };
 
-		var events = [
-			{
-				date: currentMonth + "-" + "1",
-				title: "Cat Frisbee",
-				location: "Jefferson Park"
-			},
-			{
-				date: currentMonth + "-" + "6",
-				title: "Lorem ipsum!",
-				location: "Center for Beautiful Cats"
-			},
-			{
-				date: currentMonth + "-" + "10",
-				title: "Persian Kitten Auction",
-				location: "Center for Beautiful Cats"
-			},
-			{
-				date: currentMonth + "-" + "19",
-				title: "Dolor sit amet.",
-				location: "Center for Beautiful Cats"
-			},
-			{
-				date: currentMonth + "-" + "24",
-				title: "Her birthday!",
-				location: "Center for Beautiful Cats"
-			},
-			{
-				date: currentMonth + "-" + "29",
-				title: "My day!",
-				location: "Center for Beautiful Cats"
-			},
-			{
-				date: nextMonth + "-" + "07",
-				title: "Small Cat Photo Session",
-				location: "Center for Cat Photography"
-			}
-		]
+    const currentMonth = moment().format('YYYY-MM');
+    const nextMonth = moment().add(1, 'month').format('YYYY-MM');
 
-		var miniClndr = $("#mini-clndr")
-		miniClndr.clndr({
-			template: $("#mini-clndr-template").html(),
-			daysOfTheWeek: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-			events: events,
-			clickEvents: {
-				click: function (target) {
-					if (target.events.length) miniClndr.toggleClass("show-events", true)
-					miniClndr.find(".x-button").click(function () {
-						miniClndr.toggleClass("show-events", false)
-					})
-				},
-				onMonthChange: function () {
-					miniClndr.toggleClass("show-events", false)
-					miniClndr.find(".x-button").click(function () {
-						miniClndr.toggleClass("show-events", false)
-					})
-				}
-			},
-			adjacentDaysChangeMonth: true,
-			forceSixRows: false
-		})
-	}
+    const events = [
+      {
+        date: `${currentMonth}-1`,
+        title: 'Cat Frisbee',
+        location: 'Jefferson Park',
+      },
+      {
+        date: `${currentMonth}-6`,
+        title: 'Lorem ipsum!',
+        location: 'Center for Beautiful Cats',
+      },
+      {
+        date: `${currentMonth}-10`,
+        title: 'Persian Kitten Auction',
+        location: 'Center for Beautiful Cats',
+      },
+      {
+        date: `${currentMonth}-19`,
+        title: 'Dolor sit amet.',
+        location: 'Center for Beautiful Cats',
+      },
+      {
+        date: `${currentMonth}-24`,
+        title: 'Her birthday!',
+        location: 'Center for Beautiful Cats',
+      },
+      {
+        date: `${currentMonth}-29`,
+        title: 'My day!',
+        location: 'Center for Beautiful Cats',
+      },
+      {
+        date: `${nextMonth}-07`,
+        title: 'Small Cat Photo Session',
+        location: 'Center for Cat Photography',
+      },
+    ];
 
-	// Google GeoChart Visualization
-	if ($('#geo-map').length) {
-		google.charts.load('current', {
-			'packages': ['geochart'],
-			'mapsApiKey': 'AIzaSyCnO5ud0AQXw38v6CWmNujOeksxvjqUdfk'
-		})
-		google.charts.setOnLoadCallback(drawRegionsMap)
+    const miniClndr = $('#mini-clndr');
+    miniClndr.clndr({
+      template: $('#mini-clndr-template').html(),
+      daysOfTheWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+      events,
+      clickEvents: {
+        click(target) {
+          if (target.events.length) miniClndr.toggleClass('show-events', true);
+          miniClndr.find('.x-button').click(() => {
+            miniClndr.toggleClass('show-events', false);
+          });
+        },
+        onMonthChange() {
+          miniClndr.toggleClass('show-events', false);
+          miniClndr.find('.x-button').click(() => {
+            miniClndr.toggleClass('show-events', false);
+          });
+        },
+      },
+      adjacentDaysChangeMonth: true,
+      forceSixRows: false,
+    });
+  }
 
-		function drawRegionsMap() {
-			var data = google.visualization.arrayToDataTable([
-				['Provinces', 'Region'],
-				[{ v: 'PH-01', f: 'Metallic Mining' }, 1],
-				[{ v: 'PH-03', f: 'Non-Metallic Mining' }, 3],
-				[{ v: 'PH-41', f: 'Oil & Gas' }, 4],
-				[{ v: 'PH-05', f: 'Metallic Mining' }, 5],
-				[{ v: 'PH-06', f: 'Renewable Energy Source' }, 6],
-				[{ v: 'PH-07', f: 'Metallic Mining' }, 7],
-				[{ v: 'PH-13', f: 'Metallic Mining' }, 13]
-			])
+  // Google GeoChart Visualization : https://www.gstatic.com/charts/loader.js
+  if ($('#geo-map').length) {
+    const drawRegionsMap = function () {
+      const data = google.visualization.arrayToDataTable([
+        ['Provinces', 'Region'],
+        [{ v: 'PH-01', f: 'Metallic Mining' }, 1],
+        [{ v: 'PH-03', f: 'Non-Metallic Mining' }, 3],
+        [{ v: 'PH-41', f: 'Oil & Gas' }, 4],
+        [{ v: 'PH-05', f: 'Metallic Mining' }, 5],
+        [{ v: 'PH-06', f: 'Renewable Energy Source' }, 6],
+        [{ v: 'PH-07', f: 'Metallic Mining' }, 7],
+        [{ v: 'PH-13', f: 'Metallic Mining' }, 13],
+      ]);
 
-			var options = {
-				region: 'PH',
-				resolution: 'provinces',
-				backgroundColor: '#fff',
-				datalessRegionColor: '#9CCC65',
-				colorAxis: {
-					colors: ['#FFFF03', '#FF0000', '#000099', '#FFFF03', '#009900', '#FFFF03', '#FFFF03'],
-					values: [1, 3, 4, 5, 6, 7, 13]
-				},
-				legend: 'none'
-			}
+      const options = {
+        region: 'PH',
+        resolution: 'provinces',
+        backgroundColor: '#fff',
+        datalessRegionColor: '#9CCC65',
+        colorAxis: {
+          colors: ['#FFFF03', '#FF0000', '#000099', '#FFFF03', '#009900', '#FFFF03', '#FFFF03'],
+          values: [1, 3, 4, 5, 6, 7, 13],
+        },
+        legend: 'none',
+      };
 
-			var chart = new google.visualization.GeoChart(document.getElementById('geo-map'))
-			chart.draw(data, options)
+      const chart = new google.visualization.GeoChart(document.getElementById('geo-map'));
+      chart.draw(data, options);
 
-			// Chart Responsiveness
-			$(window).resize(() => chart.draw(data, options))
-		}
-	}
+      // Chart Responsiveness
+      $(window).resize(() => chart.draw(data, options));
+    };
 
-	// NanoGallery
-	if ($('#nano-gallery').length) {
-		$('#nano-gallery').nanogallery2({
-			displayBreadcrumb: false,
-			thumbnailDisplayTransition: 'slideLeft',
-			thumbnailDisplayTransitionDuration: 500,
-			thumbnailDisplayInterval: 250
-		})
+    google.charts.load('current', {
+      callback: drawRegionsMap,
+      packages: ['geochart'],
+      mapsApiKey: 'AIzaSyCnO5ud0AQXw38v6CWmNujOeksxvjqUdfk',
+    });
+  }
 
-		$('.nGY2Gallery').one('click', function (e) {
-			if (e.target.className === 'nGY2GThumbnailCustomLayer') {
-				var mLayer = e.target
-				var h2 = $('#mAlbum')
-				var albumName = $(mLayer).next().children().text()
+  // NanoGallery
+  if ($('#nano-gallery').length) {
+    $('#nano-gallery').nanogallery2({
+      displayBreadcrumb: false,
+      thumbnailDisplayTransition: 'slideLeft',
+      thumbnailDisplayTransitionDuration: 500,
+      thumbnailDisplayInterval: 250,
+    });
 
-				if (albumName.indexOf('image') !== -1) return
+    $('.nGY2Gallery').one('click', (e) => {
+      if (e.target.className === 'nGY2GThumbnailCustomLayer') {
+        const mLayer = e.target;
+        const h2 = $('#mAlbum');
+        const albumName = $(mLayer).next().children().text();
 
-				h2.html(albumName)
-				h2.toggleClass('mDisplay')
-			}
-		})
-	}
-})(window.jQuery)
+        if (albumName.indexOf('image') !== -1) return;
+
+        h2.html(albumName);
+        h2.toggleClass('mDisplay');
+      }
+    });
+  }
+}(window.jQuery));
